a4b0e82295a4f690a720ee73b00998d5ef40d84a
Leonor Perez <leonzia_8@hotmail.com>
2022-10-08 00:03:29 -0300

    fist commit

---
+++ src/test/java/com/cervantes/CervantesBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cervantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CervantesBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
